5d1ca9c3a2f95be58117ccdaf116b144f30faa3a
jrsussner <jrsussner@gmail.com>
2025-09-23 17:10:10 -0400

    FEAT - Set up div containers for each item on the home page

--- tampapalms/src/app/pages/Home/page.tsx
+++ tampapalms/src/app/pages/Home/page.tsx
@@ -1,8 +1,22 @@
 export default function Home(){
     return (
       <div>
-        <div className="rounded-xl bg-gray-200 m-8 p-6 shadow-md">
-            <h1 className="text-black">Home Page</h1>
+        {/* Highlighted Images container */}
+        <div className="relative h-[80dvh] rounded-xl bg-gray-200 my-2 mx-8 p-6">
+          <h1 className="text-black">Highlighted Images</h1>
+          <div className="absolute bottom-0 right-0 h-50 w-50 bg-white rounded-xl">
+            <p className="text-black text-center pt-4">Highlighted Location</p>
+          </div>
+        </div>
+        {/* Explore Spaces container */}
+        <div className="h-[80dvh] rounded-xl mt-8 mx-8 p-6">
+          <h1 className="text-black text-center text-3xl">Explore Spaces</h1>
+          <hr className="h-px mt-2 border-t-3 border-black rounded-4xl"></hr>
+          <div className="w-full flex flex-row justify-center">
+            {/* LoopNet locations */}
+            <div className="h-[50dvh] w-[50dvh] bg-gray-200 hover:bg-gray-800 transition-colors duration-300 ease-in-out m-2 rounded-xl"></div>
+            <div className="h-[50dvh] w-[50dvh] bg-gray-200 hover:bg-gray-800 transition-colors duration-300 ease-in-out m-2 rounded-xl"></div>
+          </div>
         </div>
       </div>
     );
