1786211931a983f83cfd10e356516014368af0cb
Khalil Smith <khalil.smith@spartans.ut.edu>
2025-09-30 13:52:23 -0400

    FIX- Deleted these files due to the wrong format

--- tampapalms/OfficeApplication.tsx
+++
@@ -1,8 +0,0 @@
-const container = document.getElementById("image-container");
-if (container) {
-    const img = document.createElement("img");
-    img.src = "5331-Primrose-Lake-Cir-Tampa-FL-Aerial-1-LargeHighDefinition.jpg"
-    container.appendChild(img);
-}
-
-
