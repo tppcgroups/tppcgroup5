4a208c398f82a1c855f9735993a7998a12d2d1bd
Khalil Smith <khalil.smith@spartans.ut.edu>
2025-09-30 13:37:48 -0400

    FEAT-Added the Application Title

---
+++ tampapalms/src/app/components/RentalApplication/ApplicationTitle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ApplicationTitle: React.FC = () => {
+    return (
+        <div className="bg-foreground text-background rounded-2xl flex items-center justify-center text-lg font-medium w-full max-w-md h-28 mb-12">
+            Your New Office Starts Here!
+        </div>
+    );
+};
+
+export default ApplicationTitle;
+
