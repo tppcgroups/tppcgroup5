0de5636fc3389ea1eedc413a2a598af88ad0b3db
Khalil Smith <khalil.smith@spartans.ut.edu>
2025-09-30 15:04:19 -0400

    FEAT- Added a shadow behind the leasing application link

--- tampapalms/src/app/components/RentalApplication/ApplicationLink.tsx
+++ tampapalms/src/app/components/RentalApplication/ApplicationLink.tsx
@@ -7,6 +7,9 @@ const ApplicationLink: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
             className="bg-foreground text-background rounded-2xl flex items-center justify-center text-lg font-medium w-full max-w-md h-28 mb-12 shadow-2x1 underline underline-offset-4 mt-8 hover:opacity-80 transition"
+            style={{
+                boxShadow: "0 0 20px 4px rgba(128, 128, 128, 0.4)", // subtle gray glow
+            }}
         >
             Let’s Begin Your Application
         </a>
