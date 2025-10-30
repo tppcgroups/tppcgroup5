d1337f5e75f3d6379d1c7fe5a3afe372565e1d78
jrsussner18 <jrsussner@gmail.com>
2025-10-30 12:51:23 -0400

    FEAT - Made text size on nav bar smaller on smaller screens, hard cutoff at 900px

--- tampapalms/src/app/components/header/NavLinks.tsx
+++ tampapalms/src/app/components/header/NavLinks.tsx
@@ -30,12 +30,12 @@ const NavLinks: React.FC<NavLinksProps> = ({ setIsOpen }) => {
   return (
     // This component now renders the links for either desktop or mobile
     // The parent (Header.tsx) decides WHEN to show it.
-    <div className="flex flex-col md:flex-row md:flex-1 md:justify-evenly items-center md:space-x-4 space-y-4 md:space-y-0 py-4 md:py-0">
+    <div className="flex flex-col min-[800px]:flex-row min-[900px]:flex-1 min-[800px]:justify-evenly items-center min-[800px]:space-x-4 space-y-4 min-[800px]:space-y-0 py-4 min-[800px]:py-0">
       {navLinks.map((link) => (
         <Link key={link.href} href={link.href} passHref>
           <span
             onClick={handleLinkClick}
-            className={`relative font-bold text-xl py-2 text-gray-800 hover:text-black after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:h-[2px] after:w-0
+            className={`relative font-bold text-[10px] min-[1110px]:text-[15px] min-[1300px]:text-xl py-2 text-gray-800 hover:text-black after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:h-[2px] after:w-0
               after:bg-gray-800 after:transition-all after:duration-500
               hover:after:w-full ${
               pathname === link.href
